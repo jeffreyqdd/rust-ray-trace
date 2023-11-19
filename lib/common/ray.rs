4b827c328f6b5ca8985458acda126a1adb5b2fa3
Jeffrey Qian <jq54@cornell.edu>
2023-11-19 17:46:18 -0500

    AbsDiffEq and RelativeEq implemented for Ray

--- lib/common/ray.rs
+++ lib/common/ray.rs
@@ -1,5 +1,7 @@
+use approx::{abs_diff_eq, relative_eq, AbsDiffEq, RelativeEq};
 use nalgebra::{Point3, Vector3};
 
+#[derive(Debug, PartialEq)]
 pub struct Ray {
     pub origin: Point3<f64>,
     pub direction: Vector3<f64>,
@@ -19,5 +21,54 @@ impl Ray {
 
     pub fn at(&self, t: f64) -> Point3<f64> {
         self.origin + self.direction * t
+    }
+}
+
+impl AbsDiffEq for Ray {
+    type Epsilon = f64;
+
+    fn default_epsilon() -> Self::Epsilon {
+        1e-14
+    }
+
+    fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        abs_diff_eq!(self.origin, other.origin, epsilon = epsilon)
+            && abs_diff_eq!(self.direction, other.direction, epsilon = epsilon)
+            && abs_diff_eq!(self.start, other.start, epsilon = epsilon)
+            && abs_diff_eq!(self.end, other.end, epsilon = epsilon)
+    }
+}
+impl RelativeEq for Ray {
+    fn default_max_relative() -> Self::Epsilon {
+        Self::default_epsilon()
+    }
+
+    fn relative_eq(
+        &self,
+        other: &Self,
+        epsilon: Self::Epsilon,
+        max_relative: Self::Epsilon,
+    ) -> bool {
+        relative_eq!(
+            self.origin,
+            other.origin,
+            epsilon = epsilon,
+            max_relative = max_relative
+        ) && relative_eq!(
+            self.direction,
+            other.direction,
+            epsilon = epsilon,
+            max_relative = max_relative
+        ) && relative_eq!(
+            self.start,
+            other.start,
+            epsilon = epsilon,
+            max_relative = max_relative
+        ) && relative_eq!(
+            self.end,
+            other.end,
+            epsilon = epsilon,
+            max_relative = max_relative
+        )
     }
 }
