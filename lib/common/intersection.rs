0a549eca6159fdf998d10fa189952a0a20b1d3d7
Jeffrey Qian <jq54@cornell.edu>
2023-11-19 17:43:29 -0500

    Remove custom PartialEq for IntersectResult and use derive implementation

--- lib/common/intersection.rs
+++ lib/common/intersection.rs
@@ -1,6 +1,6 @@
-use nalgebra::{distance, Point3, Unit, Vector3};
+use nalgebra::{Point3, Unit, Vector3};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum IntersectResult {
     Hit {
         t: f64,
@@ -8,29 +8,4 @@ pub enum IntersectResult {
         normal: Unit<Vector3<f64>>,
     },
     Miss,
-}
-
-impl PartialEq for IntersectResult {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (
-                IntersectResult::Hit {
-                    t: t1,
-                    point: p1,
-                    normal: n1,
-                },
-                IntersectResult::Hit {
-                    t: t2,
-                    point: p2,
-                    normal: n2,
-                },
-            ) => {
-                let t_diff = f64::abs(t1 - t2);
-                let p_diff = distance(p1, p2);
-                t_diff < 1e-8 && p_diff < 1e-8 && n1 == n2
-            }
-            (IntersectResult::Miss, IntersectResult::Miss) => true,
-            _ => false,
-        }
-    }
 }
