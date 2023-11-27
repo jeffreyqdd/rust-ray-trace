160f384c1fdc74265004c3b9e615a7b5d2aa447b
Jeffrey Qian <jq54@cornell.edu>
2023-11-27 13:03:57 -0500

    add documentation to ray struct and impl

--- lib/common/ray.rs
+++ lib/common/ray.rs
@@ -3,16 +3,23 @@ use approx::{abs_diff_eq, relative_eq, AbsDiffEq, RelativeEq};
 use nalgebra::{Point3, Vector3};
 
 #[derive(Debug, PartialEq)]
+/// Represents a ray in the form o + dt where o is the origin, d is the
+/// direction and t is the parametric variable
 pub struct Ray {
+    /// point on ray when t = 0
     pub origin: Point3<f64>,
+
+    /// direction of ray
     pub direction: Vector3<f64>,
 }
 
 impl Ray {
+    /// Construct a ray struct the provided origin and direction
     pub fn new(origin: Point3<f64>, direction: Vector3<f64>) -> Ray {
         Ray { origin, direction }
     }
 
+    /// evaluates the ray equation and returns the point on P(t)
     pub fn at(&self, t: f64) -> Point3<f64> {
         self.origin + self.direction * t
     }
