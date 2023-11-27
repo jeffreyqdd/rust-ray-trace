3945fd2b910ccd8bcfcd0fda1e7d286a98014b37
Jeffrey Qian <38968278+jeffreyqdd@users.noreply.github.com>
2023-11-27 13:05:59 -0500

    Ray update (#2)
    * replace magic number in Ray.rs with global TRACE_EPSILON
    
    * add documentation to ray struct and impl

--- lib/common/ray.rs
+++ lib/common/ray.rs
@@ -1,17 +1,25 @@
+use crate::TRACE_EPSILON;
 use approx::{abs_diff_eq, relative_eq, AbsDiffEq, RelativeEq};
 use nalgebra::{Point3, Vector3};
 
 #[derive(Debug, PartialEq)]
+/// Represents a ray in the form o + dt where o is the origin, d is the
+/// direction and t is the parametric variable
 pub struct Ray {
+    /// point on ray when t = 0
     pub origin: Point3<f64>,
+
+    /// direction of ray
     pub direction: Vector3<f64>,
 }
 
 impl Ray {
+    /// Construct a ray struct the provided origin and direction
     pub fn new(origin: Point3<f64>, direction: Vector3<f64>) -> Ray {
         Ray { origin, direction }
     }
 
+    /// evaluates the ray equation and returns the point on P(t)
     pub fn at(&self, t: f64) -> Point3<f64> {
         self.origin + self.direction * t
     }
@@ -21,7 +29,7 @@ impl AbsDiffEq for Ray {
     type Epsilon = f64;
 
     fn default_epsilon() -> Self::Epsilon {
-        1e-14
+        TRACE_EPSILON
     }
 
     fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
