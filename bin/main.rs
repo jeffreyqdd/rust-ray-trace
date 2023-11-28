dd495cf6b2f5acdd21aab03f32e09bfa55b79294
Jeffrey Qian <38968278+jeffreyqdd@users.noreply.github.com>
2023-11-28 13:49:41 -0500

    add scene to hold objects (#5)

--- bin/main.rs
+++ bin/main.rs
@@ -1,30 +1,27 @@
 use camera::{Camera, PerspectiveCamera};
-use common::IntersectResult;
-use geometry::{Plane, Surface};
+use common::{IntersectResult, Scene};
+use geometry::{Plane, Sphere, Surface};
 use nalgebra::{point, vector, UnitVector3};
 use ray_tracer::*;
 
 fn main() {
     let camera = PerspectiveCamera::new(
         1.,
-        point![0., 0., 0.],
-        point![1., 0., 0.,],
+        point![3., 0., 0.],
+        point![-1., 0., 0.,],
         vector![0., 1., 0.],
         25.,
         16. / 9.,
     );
 
-    let plane = Plane::new(
-        point![0., -20., 0.,],
-        UnitVector3::new_normalize(vector![0.1, 1., 0.2]),
-    );
-
-    // println!("{:#?}", plane);
-    // println!("{:#?}", plane);
-
-    let image_width = 560;
-    let image_height = 560;
+    let image_width = 256;
+    let image_height = 144;
     print!("P3\n{} {}\n{}\n", image_width, image_height, 255);
+
+    // create scene
+    let sphere1 = Sphere::new(point![-1., 0., 0.], 0.5);
+    let sphere2 = Sphere::new(point![0., -100.5, 0.], 100.);
+    let scene = Scene::new(vec![Box::new(sphere1), Box::new(sphere2)]);
 
     for j in 0..image_height {
         for i in 0..image_width {
@@ -35,23 +32,26 @@ fn main() {
                 (c + 0.5) / (image_width as f64),
                 (r + 0.5) / (image_height as f64),
             );
-            let hit = plane.intersect(&ray);
 
-            match hit {
-                IntersectResult::Hit { point, .. } => {
-                    let scale = 0.01;
-                    let pattern = (((scale * f64::abs(point.x)) % 1.) > 0.5)
-                        ^ (((scale * f64::abs(point.z)) % 1.) > 0.5);
-                    if pattern {
-                        println!("255 255 255");
-                    } else {
-                        println!("55 55 55");
-                    }
+            // x y z
+            // x z y
+
+            // y x z
+            // y z x
+
+            // z x y
+            // z y x
+            match scene.intersect(&ray) {
+                IntersectResult::Hit { t, point, normal } => {
+                    println!(
+                        "{} {} {}",
+                        ((normal.z * 255. + 255.) * 0.5) as u8,
+                        ((normal.y * 255. + 255.) * 0.5) as u8,
+                        ((normal.x * 255. + 255.) * 0.5) as u8
+                    );
                 }
-                _ => {
-                    println!("0 0 0")
-                }
-            };
+                IntersectResult::Miss => println!("0 0 0"),
+            }
         }
     }
 }
